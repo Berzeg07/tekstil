9514efb7d7e6cd369600e15922c65053ff37379c
Berzeg07 <berezg86@gmail.com>
2017-06-17 22:47:28 +0400

    17.06

---
+++ contact.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Контакты</title>
+	<?php require_once('includes/header.php'); ?>
+</head>
+<body>
+
+
+	<?php require_once('includes/header_top.php'); ?>
+
+	<div class="container">
+
+		<div class="header-img header-img__contact">
+			<ul>
+				<li>КАЧЕСТВЕННАЯ ПРОДУКЦИЯ</li>
+				<li>НИЗКИЕ ЦЕНЫ</li>
+				<li>РАБОТА НА ВАШЕМ <br/>МАТЕРИАЛЕ</li>
+			</ul>
+			<div class="header-img__button">
+				<a class="button1" href="#">Заказать</a>
+			</div>
+		</div>
+
+		<div class="complect-title">
+			<h1>Контакты</h1>
+		</div>
+
+		<div class="complect-content">
+
+			<p class="content-main">
+				Здесь указать часы работы 
+			</p>
+
+			<div class="contact-box">
+
+				<div class="contact-box__left">
+					<div class="contact-box__left_item">
+						<span>Телефоны для связи</span><br/>
+						<span class="cont-sp cont-sp1">+7 (925) 104-58-04 </span><br/>
+						<span class="cont-sp">+7 (926) 101-94-08</span>
+					</div>
+					<div class="contact-box__left_item">
+						<span>E-MAIL:</span><br/>
+						<span class="cont-sp">altik@inbox.ru</span>
+					</div>
+					<div class="contact-box__left_item">
+						<span>Адрес:</span><br/>
+						<span class="cont-sp">
+							Россия, Московская область<br/> 
+							г.Мытищи
+						</span>
+					</div>
+				</div>
+
+				<div class="contact-box__right">
+					<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ab62f60c26871c9394bd9cd812c748210eb6fb779b7ff2eefd1759ef9f82dff1d&amp;width=100%25&amp;height=520&amp;lang=ru_RU&amp;scroll=false"></script>
+				</div>
+				<div class="clear"></div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<?php require_once('includes/footer.php'); ?>
+
+
+</body>
+</html>
