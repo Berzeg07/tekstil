13f6f4e16080c546d96c4e6f088cf0d41b724863
Berzeg07 <berezg86@gmail.com>
2017-06-17 21:36:10 +0400

    17.06

---
+++ actions.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Акция</title>
+	<?php require_once('includes/header.php'); ?>
+</head>
+<body>
+
+
+	<?php require_once('includes/header_top.php'); ?>
+
+	<div class="container">
+
+		<div class="header-img header-img__actions">
+
+			<div class="action-title">
+				<span>Акция</span><br/>
+				<span>ликвидация склада стеганой подкладки</span>
+			</div>
+
+			<div class="header-img__button">
+				<a class="button1" href="#">Заказать</a>
+			</div>
+
+		</div>
+
+
+		<div class="content__actions">
+			<h1>Акция</h1>
+			<p class="content-main">
+				В связи с ликвидацией склада стеганой подкладки, наша компания производит распродажу остатков стеганой подкладки.
+			</p>
+			<div class="action-box">
+				<div class="action-box__item">
+					<span>В наличии есть:</span>
+					<ul class="action-box__firstul">
+						<li>светло-бежевая стеганая подкладка на ватине</li>
+					</ul>
+					<p class="content-main">Стеганая подкладка на синтепоне:</p>
+					<ul class="action-box__secondul">
+						<li>светло-бежевая,</li>
+						<li>коричневая,</li>
+						<li>болото,</li>
+						<li>черная,</li>
+						<li>белая,</li>
+						<li>темно-зеленая</li>
+					</ul>
+				</div>
+				<div class="action-box__item">
+					<div class="form-box form-box__actions">
+						<form action="#">
+							<p>
+								ВЫ МОЖЕТЕ ОСТАВИТЬ<br/> 
+								ЗАЯВКУ ПРЯМО СЕЙЧАС
+							</p>
+							<p class="pf">введите данные в поле ниже</p>
+							<span>Имя*</span>
+							<input type="text">
+							<span>Email*</span>
+							<input type="text">
+							<div class="form-but">
+								<button>Отправить</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<?php require_once('includes/footer.php'); ?>
+
+
+</body>
+</html>
