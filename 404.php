322ee4e07a28c2de814f26c0d65935943d6a8ade
Berzeg07 <berezg86@gmail.com>
2017-06-18 19:27:53 +0400

    18.06

---
+++ 404.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Контакты</title>
+	<?php require_once('includes/header.php'); ?>
+</head>
+<body class="error-wrap">
+
+
+
+
+		<div class="container">
+
+			<div class="error-box">
+				<span>404</span>
+				<p>Страница не найдена или удалена</p>
+				<a class="error-but1" href="#">Вернуться назад</a>
+				<a class="error-but2" href="#">На главную</a>
+			</div>
+
+		</div>
+	
+
+	
+
+
+</body>
+</html>
