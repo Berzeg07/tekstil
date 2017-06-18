dd30f0c1ff50be6bb27cf3268d666a1ba18bbe3c
engine77 <zaur.1kager@gmail.com>
2017-06-18 16:20:05 +0400

    product

--- product.php
+++ product.php
@@ -26,53 +26,292 @@
 
 		<div class="complect-title complect-title__product">
 			<h1>Продукция:</h1>
-		</div>
-
-		<div class="complect-content about-content">
-
-			<p class="content-main">
-				Наше предприятие оснащено многоигольчатыми стегальными машинами итальянской фирмы «Meca», которые способны выполнять более пятисот рисунков. 
-				Подбор рисунка может быть осуществлен по желанию заказчика на любой вкус. Основные их типы доступны на нашем сайте в виде изображений. Наше оборудование 
-				компьютерезировано, что позволяет выполнять на нем самые сложные рисунки. Толщина пакета может составлять до 4см.  Производительность такого оборудования составляет 
-				50-60 метров в час, в зависимости от сложности рисунка. <br>
-				Все оборудование подвергается постоянному техническому контролю и своевременному обслуживанию. 
-			</p>					
-		</div>
-
-		
-		
-
-		<div class="complect-content about-content">
-			<p class="content-main">
-				Подбор рисунка может быть осуществлен по желанию заказчика на любой вкус. Основные их типы доступны на нашем сайте в виде изображений. Наше 
-			</p>				
-		</div>
-
-		<div class="machine__formbox">
-			<div class="machine__leftimg">
-				<img src="img/machineimg.jpg" alt="">
+			<div class="product__tabs">
+				<a href="#" class="product__activetab"><span>Ткани</span></a>
+				<a href="#"><span>Стежка на <br> материале заказчика</span></a>
+				<a href="#"><span>Матрасная стежка</span></a>
 			</div>
-			<div class="form-box form-box__about">
-				<form action="#">
-					<p>
-						ВЫ МОЖЕТЕ ОСТАВИТЬ<br> 
-						ЗАЯВКУ ПРЯМО СЕЙЧАС
-					</p>
-					<p class="pf">введите данные в поле ниже</p>
-					<span>Имя*</span>
-					<input type="text">
-					<span>Email*</span>
-					<input type="text">
-					<div class="form-but">
-						<button>Отправить</button>
+		</div>
+		<div class="complect-content about-content">
+			<p class="content-main">
+				Специализация нашей компании - производство стеганого полотна, приоритетное направление, на которое приходится основная доля продукции, - стеганое матрасное полотно или, как его
+				еще называют,  матрасная стежка.
+			</p>
+		</div>
+
+		<div class="product__tab-block">		
+			<div class="complect-content about-content">
+				<h2>Стеганное полотно </h2>	
+				<p class="content-main">Что же из себя представляет стеганое полотно? Для начала стоит уточнить, что стеганое полотно на разных ресурсах вы можете найти под другими названиями, к примеру, стеганная 
+					ткань - разницы нет никакой абсолютно, не путайтесь. Такое полотно представляет собой два сшитых между собой куска материи, между которыми находится наполнитель. 
+					В качестве наполнителя используют, как правило, синтепон, поролон, ватин, шерсть, хлопкофон и другие. 
+					Для внешнего вида изделия очень важен подбор рисунка стежки, который зависит от толщины пакета. Узорная стежка ткани отлично украшает покрывало или одеяло, если на их лицевой 
+					\стороне находится цельный материал. Стеганный узор можно разнообразить, если использовать нитки совершенно разных цветов и оттенков, а также придумывая новые рисунки для стежки. 
+					Но это не самое главное, не то, что в конечном итоге может определить внешний вид изделия. Все зависит от стежки. Лицевая сторона выполненного изделия может быть оформлена в 
+					разных техниках, в 
+				</p>				
+			</div>		
+			<div class="product__sliderblock">
+				<div class="owl-product-carousel">
+					<div class="item">
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product1.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product2.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product3.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product4.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product5.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product6.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product7.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product8.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
 					</div>
-				</form>
+					<div class="item">
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product1.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product2.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product3.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product4.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product5.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product6.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product7.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product8.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+					</div>
+					<div class="item">
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product1.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product2.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product3.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product4.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product5.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product6.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product7.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+						<a href="#" class="stitches__link product__link">
+							<div class="stitches__img">
+								<img src="img/product8.jpg" alt="">
+							</div>
+							<div class="stitches__hoverbox">
+								<h4>Велюр</h4>	
+								<span>antistress</span>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>			
+			<div class="product__bynowbox">
+				<h2> Вы можете заказать продукт прямо сейчас </h2>
+				<a href="#" class="button1">ЗАКАЗАТЬ</a>
 			</div>
+		</div>	
+
+		<div class="complect-content about-content">
+			<h2>Технология стежки</h2>	
+			<p class="content-main">Технология стежки для оформления одеяла, покрывала или любой другой вещи тоже выбирается индивидуально, после того, как выбрана ткань и орнамент для стежки. Это может быть 
+				ручная работа, которая особенно ценилась во все времена, или современное высокотехнологичное стегальное производство, где используется для обработки различных тканей, например, 
+				многоигольная стегальная машина «Меса», используемая нашей компанией, и другое стегальное оборудование. Машины итальянской фирмы «Меса» способны выполнять более 500 
+				рисунков, и это помогает подобрать рисунок на любой, самый взыскательный вкус. Основные рисунки стежки можно увидеть на нашем сайте. 
+			</p>	
+			
+			<h2>Стежка матрасов</h2>
+
+			<p class="content-main">Особое значение выбор стеганого полотна имеет при изготовлении матрасов. Стежка матрасов и наматрасников традиционно производится методом глубокой объемной стежки на 
+				специальном оборудовании. И простеганные узоры всегда выглядят на матрасах очень привлекательно. Эта продукция востребована покупателем, поэтому объемы ее производства 
+				наращиваются год от года и растет количество наших клиентов. Появляются все новые производители матрасов, отечественные и зарубежные, которым мы поставляем все новые 
+				качественные образцы стеганого матрасного полотна. В зависимости от класса и уровня матраса в его изготовлении используются разные полотна: креп-сатин, синтепон, поликоттон и 
+				другие. Мы применяем новые современные качественные утеплители, благодаря использованию которых стеганое полотно становится еще более мягким, эластичным и, как следствие, 
+				более комфортным для человеческого тела. Часто сегодня применяют для прокладывания кусков материи поролон (8 и 15 мм), различной плотности синтепон, шерсть, шерстипон и 
+				хопкофон, а также ватин, войлок и ультрановый, недавно разработанный технологами, сверхпрочный и легкий материал — "спанбонд". Можно осуществить и комбинирование различных 
+				материалов. Кроме того, стеганные материалы широко используются для производства покрывал, одеял, подушек, штор, верхней одежды и даже мебели. Сегодня простегать можно самые 
+				разные типы тканей - отлично выглядит, например, жаккард стеганный или хлопковое стеганное покрывало из натурального коттона.
+			</p>
+
 		</div>
+
+
 
 	</div>
 
-	
+
 
 	<?php require_once('includes/footer.php'); ?>
 
